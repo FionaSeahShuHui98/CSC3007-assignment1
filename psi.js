0ef2867c24952d6be8248d75787ece6633b09d64
FionaSeahShuHui98 <1901878@sit.singaporetech.edu.sg>
2022-05-28 16:54:11 +0800

    Final Submission

--- psi.js
+++ psi.js
@@ -8,7 +8,8 @@ fetch ("https://api.data.gov.sg/v1/environment/psi").then ((psi_data) => {
 
     // Conversion from JSON to Object
     return psi_data.json();
-}).then((psi_object) => {
+
+}).then(function(psi_object) {
 
     // To ensure that all data has been coverted to Object //
     // console.log (psi_object);
@@ -20,28 +21,26 @@ fetch ("https://api.data.gov.sg/v1/environment/psi").then ((psi_data) => {
     // console.log (psi_object["items"][0].update_timestamp);
 
     // Declarations //
-    var data_readings = psi_object["items"][0].readings;
-    // console.log(data_readings)
-    var data_readings_objects = Object.values(data_readings);
-    
+    var metric = Object.keys(psi_object.items[0].readings);
+    var length = metric.length;
     var time_updated = psi_object["items"][0].update_timestamp;
     // console.log(time_updated)
 
+    // PSI Data Table
+    for (var i = 0; i < length; i++) {
+        var national_col = Object.values(psi_object.items[0].readings)[i].national
+        var central_col =  Object.values(psi_object.items[0].readings)[i].central
+        var west_col =  Object.values(psi_object.items[0].readings)[i].west
+        var east_col =  Object.values(psi_object.items[0].readings)[i].east
+        var north_col =  Object.values(psi_object.items[0].readings)[i].north
+        var south_col =  Object.values(psi_object.items[0].readings)[i].south
+        $("#psi_data_table tr:last").after("<tr><td>" + metric[i] + "</td><td>" + national_col + "</td><td>" + central_col + "</td><td>" + west_col + "</td><td>" + east_col + "</td><td>" + north_col + "</td><td>" + south_col + "</td></tr>");
+    } 
 
     // Time formating
     time_updated = moment(time_updated).format("DD MMMM YYYYY, hh:mm a")
 
-    // Linking to Frontend HTML
+    // Display Update Time
     document.getElementById("time_frame").innerHTML = time_updated;
-
-    let psi_TableData = "";
-
-    data_readings_objects.map((psi_values) => {
-        psi_TableData += `
-            <tr>
-                <td>${psi_values[0]}</td>
-            </tr>
-        `
-    });
-
+    
 })
